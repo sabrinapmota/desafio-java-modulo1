2697fac699085621fec8f30cdee4e9961a4cf220
Sabrina Porto Mota de Souza <sabrinapmota@yahoo.com.br>
2022-06-30 12:14:19 -0300

    whille inserido para que usuário possa continuar jogando. Soma de pontos implementada

--- src/Jogo.java
+++ src/Jogo.java
@@ -5,23 +5,34 @@ public class Jogo {
     public static void main(String[] args) {
         Random sorteiaNumero = new Random();
         int numeroSorteado = sorteiaNumero.nextInt(50) + 1;
-    System.out.println("Adivinhe em que número estou pensando!");
-        System.out.println("Digite um número de 0 a 50: ");
-        Scanner input = new Scanner(System.in);
-        int insereNumero = input.nextInt();
-        int numeroMaior = numeroSorteado++;
-        int numeroMenor = numeroSorteado--;
-        int numeroCorreto = numeroSorteado;
-        if (insereNumero == numeroMaior) {
-            System.out.println("você ganhou 5 (cinco) pontos! ");
+        int somaPontos = 0;
+        boolean continuaJogo = true;
+        while (continuaJogo) {
 
-        } else if (insereNumero == numeroMaior) {
-            System.out.println("você ganhou 5 (cinco) pontos! ");
-        } else if (insereNumero == numeroCorreto) {
-            System.out.println("Parabéns, você acertou e ganhou 10 pontos! ");
 
-        } else {
-            System.out.println("Número incorreto, o jogo acabou. ");
+            System.out.println("Adivinhe em que número estou pensando!");
+            System.out.println("Digite um número de 0 a 50: ");
+            Scanner input = new Scanner(System.in);
+            int insereNumero = input.nextInt();
+            int numeroMaior = numeroSorteado++;
+            int numeroMenor = numeroSorteado--;
+            int numeroCorreto = numeroSorteado;
+            if (insereNumero == numeroMaior) {
+                somaPontos = somaPontos + 5;
+                System.out.println("você ganhou" + somaPontos + " pontos");
+
+            } else if (insereNumero == numeroMaior) {
+                somaPontos = somaPontos + 5;
+                System.out.println("você ganhou " + somaPontos + " pontos");
+            } else if (insereNumero == numeroCorreto) {
+                somaPontos = somaPontos + 10;
+                System.out.println("Parabéns, você acertou e ganhou " + somaPontos + " pontos");
+
+            } else {
+
+                System.out.println("Número incorreto, o jogo acabou. Seu total de pontos foi: " + somaPontos);
+                continuaJogo = false;
+            }
         }
     }
 }
