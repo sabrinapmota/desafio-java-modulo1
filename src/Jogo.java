654c4f65ad46316d596ddabf3721bd443dfeadd3
Sabrina Porto Mota de Souza <sabrinapmota@yahoo.com.br>
2022-06-30 21:42:15 -0300

    correção de bug número correto não somava dez. Implementação do sorteio de novo número quando acerta.

--- src/Jogo.java
+++ src/Jogo.java
@@ -14,20 +14,23 @@ public class Jogo {
             System.out.println("Digite um número de 0 a 50: ");
             Scanner input = new Scanner(System.in);
             int insereNumero = input.nextInt();
-            int numeroMaior = numeroSorteado++;
-            int numeroMenor = numeroSorteado--;
-            int numeroCorreto = numeroSorteado;
-            if (insereNumero == numeroMaior) {
+            int numeroMaior = numeroSorteado + 1;
+            int numeroMenor = numeroSorteado - 1;
+
+            if (insereNumero == numeroMenor) {
                 somaPontos = somaPontos + 5;
                 System.out.println("você ganhou" + somaPontos + " pontos");
 
             } else if (insereNumero == numeroMaior) {
                 somaPontos = somaPontos + 5;
                 System.out.println("você ganhou " + somaPontos + " pontos");
-            } else if (insereNumero == numeroCorreto) {
+            } else if (insereNumero == numeroSorteado) {
                 somaPontos = somaPontos + 10;
                 System.out.println("Parabéns, você acertou e ganhou " + somaPontos + " pontos");
+                numeroSorteado = sorteiaNumero.nextInt(50) + 1;
 
+                numeroMaior = numeroSorteado + 1;
+                numeroMenor = numeroSorteado - 1;
             } else {
 
                 System.out.println("Número incorreto, o jogo acabou. Seu total de pontos foi: " + somaPontos);
